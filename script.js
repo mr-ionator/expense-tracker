93402961d94fa2912dd6d92cc9c055d6b00f6b22
mr-ionator <siddh05nov@gmail.com>
2026-01-15 18:54:12 +0000

    added scripts file

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('epense tracker loaded');
+
+//variables
+const appName = "Expense Tracker";
+let totalExpenses = 0;
+let expenseCount = 0;
+
+
+console.log("App name:", appName);
+console.log("total expenses:". totalExpenses);
+console.log("number of Expenses:", expenseCount);
+
+
+//data types
+const Username = "Siddharth";
+const UserAge = 21;
+const isActive = true;
+
+console.log(typeof Username);
+console.log(typeof UserAge);
+console.log(typeof isActive);
+
+//Template literals
+const message = `welcome to ${appName}!`;
+console.log(message);
+
+//math operations
+const price1 = 10.50;
+const price2= 20.70;
+const total = price1 + price2;
+console.log(`total: ${total.toFixed(2)}eur`);
